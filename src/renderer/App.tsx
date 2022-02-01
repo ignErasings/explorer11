a1d788cfd3801288b5d779e9f654263505c24089
JuzioMiecio520 <50195535+JuzioMiecio520@users.noreply.github.com>
2022-02-01 16:09:36 +0100

    feat: acrylic window

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,48 +3,38 @@ import icon from '../../assets/icon.svg';
 import './App.css';
 
 const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <center>
+                <p style={{ color: '#ffffff' }}>Hello from Nove Explore</p>
+            </center>
+        </div>
+    );
 };
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Hello />} />
+            </Routes>
+        </Router>
+    );
 }
